adab40332a86e987173486f0c20e4d9b42d94a28
alexandrialin <alin92@uwo.ca>
2021-10-19 14:23:02 -0400

    completed connect 4 game

--- lab2-connect4/src/App.js
+++ lab2-connect4/src/App.js
@@ -1,25 +1,171 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var turns=0;
+
+function Hole(props){
+  return <div className="Hole"><div className={props.value}></div></div>
+}
+
+function Slat(props){
+    return <div className="Slat" onClick={() => props.handleClick()}>
+      {[...Array(props.holes.length)].map((x, j) => 
+        <Hole key={j} value={props.holes[j]}></Hole>)}
+      </div>
+ }
+
+class Board extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      boardState: new Array(7).fill(new Array(6).fill(null)),
+      playerTurn: 'Red',
+      gameMode: '',
+      gameSelected: false,
+      winner: ''
+    }
+  }
+
+  selectedGame(mode){
+    turns=0;
+  
+    this.setState({
+       gameMode: mode,
+       gameSelected: true, 
+       boardState: new Array(7).fill(new Array(6).fill(null))
+    })
+  }
+
+  makeMove(slatID){
+    const boardCopy = this.state.boardState.map(function(arr) {
+      return arr.slice();
+    });
+    if( boardCopy[slatID].indexOf(null) !== -1 ){
+      let newSlat = boardCopy[slatID].reverse()
+      newSlat[newSlat.indexOf(null)] = this.state.playerTurn
+      newSlat.reverse()
+      this.setState({
+        playerTurn: (this.state.playerTurn === 'Red') ? 'Yellow' : 'Red',
+        boardState: boardCopy
+      })
+    }
+
+  
+  }
+
+  /*If winner isn't declared yet*/
+  handleClick(slatID) {
+    if(this.state.winner === ''){
+      this.makeMove(slatID)
+    }
+  }
+  
+  /*check the winner*/
+  componentDidUpdate(){
+    
+    let winner = declareWinner(this.state.boardState)
+    if(this.state.winner !== winner){
+      this.setState({winner: winner})
+    } 
+    else{
+      turns++;
+    console.log(turns);
+    if(turns===43){
+      this.setState({winner: winner})
+    }
+    }
+  }
+
+  render(){
+
+    /*display colour of winner*/
+    let winnerMessageStyle
+    if(this.state.winner !== ""){
+      winnerMessageStyle = "winnerMessage appear"
+    }else {
+      winnerMessageStyle = "winnerMessage"
+    }
+
+    /*Contruct slats allocating column from board*/
+    let slats = [...Array(this.state.boardState.length)].map((x, i) => 
+      <Slat 
+          key={i}
+          holes={this.state.boardState[i]}
+          handleClick={() => this.handleClick(i)}
+      ></Slat>
+    )
+
+    return (
+      <div>
+        {this.state.gameSelected &&
+          <div className="Board">
+            {slats}
+          </div>
+        }
+        <div className={winnerMessageStyle}>{this.state.winner}</div>
+        {(!this.state.gameSelected || this.state.winner !== '') &&
+          <div>
+            <button onClick={() => this.selectedGame('start')}>Start</button>
+
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>CONNECT 4</h1>
+        </div>
+        <div className="Game">
+          <Board></Board>
+        </div>
+      </div>
+    );
+  }
+}
+
+function checkLine(a,b,c,d) {
+    return ((a !== null) && (a === b) && (a === c) && (a === d));
+}
+
+
+
+function declareWinner(bs) {
+    for (let c = 0; c < 7; c++)
+        for (let r = 0; r < 4; r++)
+            if (checkLine(bs[c][r], bs[c][r+1], bs[c][r+2], bs[c][r+3]))
+                return bs[c][r] + ' wins!'
+
+    for (let r = 0; r < 6; r++)
+         for (let c = 0; c < 4; c++)
+             if (checkLine(bs[c][r], bs[c+1][r], bs[c+2][r], bs[c+3][r]))
+                 return bs[c][r] + ' wins!'
+
+    for (let r = 0; r < 3; r++)
+         for (let c = 0; c < 4; c++)
+             if (checkLine(bs[c][r], bs[c+1][r+1], bs[c+2][r+2], bs[c+3][r+3]))
+                 return bs[c][r] + ' wins!'
+
+    for (let r = 0; r < 4; r++)
+         for (let c = 3; c < 6; c++)
+             if (checkLine(bs[c][r], bs[c-1][r+1], bs[c-2][r+2], bs[c-3][r+3]))
+                 return bs[c][r] + ' wins!'
+
+   
+if(turns>=43){
+  
+  return 'Game Over';
+}
+    return '';
 }
 
 export default App;
